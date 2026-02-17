76c6b779fd90c8c2aece645f751067a9e6a38a43
RollieGarcia0031 <garciarollie827@gmail.com>
2026-02-17 15:42:49 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { glob } from 'glob'
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      // Automatically finds every index.html in any subfolder of /src
+      input: Object.fromEntries(
+        glob.sync('src/**/index.html').map(file => [
+          file.replace('src/', '').replace('/index.html', '') || 'main',
+          resolve(__dirname, file)
+        ])
+      )
+    }
+  }
+})
