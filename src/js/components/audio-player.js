0fcacee5234efc91cc6241e8effcc2acc7164d2a
RollieGarcia0031 <garciarollie827@gmail.com>
2026-02-20 21:56:31 +0800

    update icons

--- src/js/components/audio-player.js
+++ src/js/components/audio-player.js
@@ -6,6 +6,7 @@
  * @param {string} track_src 
  * @param {string[]} track_instruments 
  * @param {string} track_description
+ * @param {string} track_genre
  * @returns 
  */
 const HTML_TEMPLATE =(
@@ -13,71 +14,121 @@ const HTML_TEMPLATE =(
     track_title,
     track_src,
     track_instruments,
-    track_description
+    track_description,
+    track_genre
 ) =>`
     <style>
       .main-body {
-          color: var(--text);
-          border: 1px solid var(--border);
-          height: 100%;
-          display: grid;
-          grid-template-rows: 1fr 1fr;
-          justify-items: center;
-          align-items: center;
-          gap: 1.5rem;
-          padding: 0 5rem;
+        color: var(--text);
+        border: 1px solid var(--border);
+        background-color: var(--bg);
+        padding: 0.5rem;
+        height: 100%;
+        display: grid;
+        grid-template-rows: 1fr auto 1fr auto;
+        justify-items: stretch;
+        align-items: center;
+        gap: 1.5rem;
       }
+
+      .title-holder {
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .title {
+        font-size: 2rem;
+        font-weight: bold;
+      }
+
+      .control-container{
+        border: 1px solid var(--border);
+        padding: 5px;
+      }
+
+      .instruments-container{
+        border-top: 1px solid var(--border);
+        padding: 0.25rem 1rem;
+      }
+
+      .instrument-list-container{
+        display: flex;
+        flex-direction: row;
+        gap: 0.5rem;
+      }
+
+      .instrument-list-container > * {
+        border: 1px solid var(--border-muted);
+        padding: 0.25rem 0.75rem;
+        border-radius: 4rem;
+        background-color: var(--bg-light);
+      }
+
+      .instrument-title{
+        font-weight: bold;
+        margin: 0.5rem 0;
+      }
+
     </style>
 
     <audio>
       <source src="${track_src}" type="audio/mpeg">
     </audio>
 
-    <div class="main-body card">
-      <div class="card f-row-start-center">
+    <div class="main-body">
+      <div class="title-holder">
         <button class="restart-btn">
-            🔄
+          <i class="bi bi-arrow-clockwise"></i>
         </button>
   
-        <p>
+        <p class="title">
           ${track_title}
         </p>
 
-        ${track_description && `
-            <p class="card">
-              ${track_description || ""}
-            </p>
-        `}
-        
       </div>
 
-      <div>
+      ${track_description && `
+        <p class="description">
+          ${track_description || ""}
+        </p>
+      `}
+
+      <p>
+        Genre: ${track_genre}
+      </p>
+        
+      <div class="control-container">
 
         <button class="stop-btn">
-            ⏹️
+          <i class="bi bi-stop-fill"></i>
         </button>
 
         <button class="back-btn">
-          ◀️
+          <i class="bi bi-skip-backward-fill"></i>
         </button>
 
         <button class="play-btn">
-          ▶️
+          <i class="bi bi-play-fill"></i>
         </button>
 
         <button class="forward-btn">
-          ▶️
+          <i class="bi bi-skip-forward-fill"></i>
         </button>
       </div>
 
-      <div class="f-row-center">
-        <p>
+      <div class="instruments-container">
+        <p class="instrument-title" >
           Instruments
         </p>
 
-        ${track_instruments.map(insrument => `
-          <div class="card">${insrument}</div>  
-        `).join('')}
+        <div class="instrument-list-container">
+          ${track_instruments.map(insrument => `
+            <div>${insrument}</div>  
+          `).join('')}
+        </div>
       </div>
 
     </div>
@@ -94,7 +145,7 @@ export class AudioPlayer extends HTMLElement {
         const track_title = this.getAttribute('track-title');
         const track_src = this.getAttribute('track-src');
 
-        this.innerHTML = HTML_TEMPLATE(track_id, track_title, track_src, this.instruments, this.description);
+        this.innerHTML = HTML_TEMPLATE(track_id, track_title, track_src, this.instruments, this.description, this.genre);
 
         this._audio = this.querySelector('audio');
 
@@ -134,7 +185,10 @@ export class AudioPlayer extends HTMLElement {
     get description(){
         return this.getAttribute('track-description') || "";
     }
-    
+
+    get genre(){
+        return this.getAttribute('track-genre') || "";
+    }
     /** Play the track */
     play(){
         const player = this.querySelector('audio');
